183a6183017a27a9c40a68236b6698f81aaa4683
HeyJun316 <55keke55@gmail.com>
2022-12-18 12:22:05 +0900

    fix: sumWithout4andNextがわからない

--- 003_practice/002_medium.js
+++ 003_practice/002_medium.js
@@ -2,30 +2,34 @@
  *  文字列のローテート
  *
  *  文字列を入力された数だけローテートさせる関数を実装してください
- *
+ *  文字列の長さー1の数
  *  example:
  *    'library',  1 => 'ylibrar'
  *    'library',  3 => 'arylibr'
  *    'library', -1 => 'ibraryl'
  *
  */
+//TODO:
+
 function rotate(str, num) {
-  let strArray = str.split('');
-  console.log(strArray);
-  let tmp = '';
-  let strLength = strArray.length;
-  console.log(strLength + 'strLen');
-  let shiftNumber = num % strLength;
-  console.log(shiftNumber + 'shiftNum');
-  if (shiftNumber < 0)
-    for (let i = 0; i < shiftNumber; i++) {
-      tmp = strArray.pop();
-      console.log(tmp + 'tmp');
-      strArray.unshift(tmp);
-      console.log(i);
-    }
-  return strArray.join('');
+  const index = num > 0 ? num : str.length + num;
+  return str.slice(str.length - index, str.length) + str.slice(0, str.length - index);
 }
+// let strArray = str.split('');
+// console.log(strArray);
+// let tmp = '';
+// let strLength = strArray.length;
+// console.log(strLength + 'strLen');
+// let shiftNumber = num % strLength;
+// console.log(shiftNumber + 'shiftNum');
+// if (shiftNumber < 0)
+//   for (let i = 0; i < shiftNumber; i++) {
+//     tmp = strArray.pop();
+//     console.log(tmp + 'tmp');
+//     strArray.unshift(tmp);
+//     console.log(i);
+//   }
+// return strArray.join('');
 
 
 
@@ -41,11 +45,18 @@ function rotate(str, num) {
  *
  */
 function removeVowels(str) {
-  const vowels = [a, i, u, e, o];
-  let resultArray = [];
+  // オブジェクト
+  const vowels = {
+    'a': true,
+    'i': true,
+    'u': true,
+    'e': true,
+    'o': true
+  }
+  let resultArray = '';
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === vowels[i]) {
-      resultArray.push(str[i]);
+    if (!vowels[str[i]]) {
+      resultArray = resultArray + str[i];
     }
     continue;
   }
@@ -64,11 +75,20 @@ function removeVowels(str) {
  *
  */
 function countStr(s1, s2) {
+  const s2Length = s2.length;
+  let counter = 0;
+  for (let i = 0; i < s1.length; i++) {
+    if (s1.substr(i, s2Length) === s2) {
+      counter++;
+    }
+  }
+  return counter;
 }
 
 /**
  *  引数に与えられたアルファベットの文字列が回文であること
  *  を確認するメソッドを実装してください
+ *  後ろと前が同じなのであってるか
  *
  *  example:
  *      work => false
@@ -79,6 +99,15 @@ function countStr(s1, s2) {
  */
 
 function isPalindrome(str) {
+  const strLength = str.length;
+  const splitStr = str.split('');
+  let strLengthFromBack = strLength - 1;
+  for (let i = 0; i < strLength; i++) {
+    if (splitStr[i] !== splitStr[strLengthFromBack - i]) {
+      return false
+    }
+  }
+  return true;
 }
 
 /**
@@ -94,27 +123,66 @@ function isPalindrome(str) {
  *    9 => False
  *    11 => True
  *
+ * 素数は 1 と それ以外の数字で割り切れない数字なので、
+11 が素数かどうかを見極めるには、
+11 / 2 で割れるか？
+11 / 3 で割れるか？
+11 / 4 で割れるか？
+11 / 5 で割れるか？
+…
+11 / 10 で割れるか？
+という形で一つずつ確認して、一つも割れるものがなければ素数と判断できます。
+一番愚直な解き方はこれになります。
+本当はもっと最適化できるのですが、とりあえずはこれをコードに直せると良いと思います。
  */
+
 function isPrime(num) {
+  if (num === 1) { return false }
+  if (num === 2) { return true }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+/**
+*  配列の4と次の数字を抜いた合計
+*
+*  与えらた配列の合計を返す関数を実装してください。
+*  ただし、配列の中に4がある場合は、4とその次の数字を合計に含めないでください。
+*
+*  example:
+*    [1, 2, 3, 4] => 6
+*    [1, 2, 3, 4, 5] => 6
+*    [1, 4, 3, 4, 5] => 1
+*    [4, 3, 3, 5] => 8
+*    [4, 3, 3, 4] => 3
+*    [4] => 0
+*
+*/
+function sumWithout4andNext(array) {
+  let withoutNextFlg = 0;
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 4) {
+      withoutNextFlg = 1;
+      continue;
+    }
+    if (withoutNextFlg === 1) {
+      count++
+      if (count >= 2)
+        continue;
+    }
+
+    sum += array[i];
+  }
+  return sum;
 }
 
-/**
- *  配列の4と次の数字を抜いた合計
- *
- *  与えらた配列の合計を返す関数を実装してください。
- *  ただし、配列の中に4がある場合は、4とその次の数字を合計に含めないでください。
- *
- *  example:
- *    [1, 2, 3, 4] => 6
- *    [1, 2, 3, 4, 5] => 6
- *    [1, 4, 3, 4, 5] => 1
- *    [4, 3, 3, 5] => 8
- *    [4, 3, 3, 4] => 3
- *    [4] => 0
- *
- */
-function sumWithout4andNext(array) {
-}
+let array = [1, 2, 3, 4];
+
+sumWithout4andNext(array);
 
 module.exports = {
   rotate,
